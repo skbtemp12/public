65d05b68b9a862810ca3d41183a8727e070dadfd
skbtemp12 <73464906+skbtemp12@users.noreply.github.com>
2021-04-17 17:30:14 +0600

    Update osintgram-setup.sh

--- osintgram-setup.sh
+++ osintgram-setup.sh
@@ -8,19 +8,7 @@ echo "\e[1;33m
 
       \e[0m" ;
 
-cd ~ ;
-
-# Old Osintgram Backup
-
-cp Osintgram Old_Ointgram ;
-cp .Osintgram Old_Osintgram_Hidden ;
-rm -rf Osintgram .Osintgram ;
-
-# Cloning and Later
-
-git clone https://github.com/Datalux/Osintgram.git ;
-mv Osintgram .Osintgram ;
-cd ~/.Osintgram ;
+# Installing Essential packages
 
 echo "\e[1;33m
 
@@ -28,8 +16,33 @@ echo "\e[1;33m
 
       \e[0m" ;
 
-sudo apt install python3 python3-pip libncurses5-dev -y ;
+sudo apt install wget git python3 python3-pip libncurses5-dev -y ;
 pip3 install -r requirements.txt ;
+
+# Old Osintgram Backup
+
+cd ~ ;
+cp Osintgram Old_Osintgram ;
+cp .Osintgram Old_Osintgram_Hidden ;
+rm -rf Osintgram .Osintgram ;
+
+echo "\e[1;33m
+
+	Dear $USER, Old Osintgram is Backed Up in Home Folder.
+
+      \e[0m" ;
+read -p "Do you want to Delete the Backup files ? [ type yes or no ] : " ANSWER
+if [ $ANSWER = "yes" ];
+then
+	rm -rf Old_Osintgram Old_Osintgram_Hidden
+fi ;
+echo ""
+
+# Cloning and Later
+
+git clone https://github.com/Datalux/Osintgram.git ;
+mv Osintgram .Osintgram ;
+cd ~/.Osintgram ;
 
 # Setup credentials
 
@@ -41,7 +54,7 @@ echo "\e[1;33m
 
 
 
-	To continue please Connect with Instagram
+	To continue please setup Instagram Credentials
 
 
  
@@ -57,25 +70,9 @@ password = $Passwd" > credentials.ini ;
 # Making Osintgram Script
 
 cd ~/.Osintgram ;
-touch osintgram ;
+rm osintgram ;
+wget https://raw.githubusercontent.com/skbtemp12/shell/main/osintgram ;
 chmod +x osintgram ;
-
-echo "
-
-#!/bin/sh
-
-cd ~/.Osintgram ;
-
-# Input prompt
-
-echo 
-echo 
-read -p 'Enter target Instagram UserID or UserName : ' TARGET;
-python3 main.py $\TARGET ;
-
-exit
-
-     " > osintgram ;
 
 # Setting up one click osintgram 
 
